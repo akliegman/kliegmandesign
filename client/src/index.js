9210d0ba470fd622ef53a74fb9468b29cefc0f37
Adam Kliegman <kliegman14@gmail.com>
2023-03-20 16:16:00 -0400

    Fix a warning due to the CSSTransition module."

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { forwardRef } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.less";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const ForwardApp = forwardRef((props, ref) => <App ref={ref} {...props} />);
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ForwardApp />
     </BrowserRouter>
   </React.StrictMode>
 );
