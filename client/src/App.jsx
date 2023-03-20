890beee4acc61c743c7b2e9667c5c3d89d658e79
Adam Kliegman <kliegman14@gmail.com>
2023-03-20 16:08:26 -0400

    Simple connection between server and client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,19 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./App.less";
 import { ResumePage } from "./pages/ResumePage";
 import { HomePage } from "./pages/HomePage";
+import { useEffect, useState } from "react";
 
 export const App = () => {
   const location = useLocation();
+
+  const [apiData, setAPIData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setAPIData(data))
+      .catch((err) => console.log(err));
+  }, [apiData]);
 
   return (
     <>
@@ -17,7 +27,7 @@ export const App = () => {
             timeout={300}
           >
             <Routes location={location}>
-              <Route exact path="/" element={<HomePage />} />
+              <Route exact path="/" element={<HomePage data={apiData} />} />
               <Route path="/resume" element={<ResumePage />} />
             </Routes>
           </CSSTransition>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 
-export const HomePage = () => {
+export const HomePage = ({ data }) => {
   return (
     <div className="HomePage">
+      <h1>Home Page</h1>
+      <p>Here is some data from the server:</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <Link to="/resume">Resume</Link>
     </div>
   );
