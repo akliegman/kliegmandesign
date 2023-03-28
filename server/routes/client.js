1051961cc5e3edb21ebaef645ca6617d92779c93
Adam Kliegman <kliegman14@gmail.com>
2023-03-28 11:53:37 -0400

    Express 404 is so fun!

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,13 +1,6 @@
 const clientRoutes = (app, path) => {
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../client/build", "index.html"),
-      (err) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 };
 
