ada79d75b5890da52f09c7f35180d0b191520281
Adam Kliegman <kliegman14@gmail.com>
2023-03-28 11:50:20 -0400

    Testing errors

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,6 +1,13 @@
 const clientRoutes = (app, path) => {
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../client/build", "index.html"),
+      (err) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
   });
 };
 
