d2011d9c6cd71c08d66d54496eaffd02d375c088
Adam Kliegman <kliegman14@gmail.com>
2023-04-03 10:03:25 -0400

    test login form

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,7 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
@@ -18,28 +14,28 @@ jest.mock("axios", () => ({
   })),
 }));
 
-const dummySession = {
-  user: {
-    email: "test@example.com",
-    firstName: "John",
-    lastName: "Doe",
-  },
-  token: "123abc",
-  isNew: false,
-};
+// const dummySession = {
+//   user: {
+//     email: "test@example.com",
+//     firstName: "John",
+//     lastName: "Doe",
+//   },
+//   token: "123abc",
+//   isNew: false,
+// };
 
-const server = setupServer(
-  rest.post("/api/login", (req, res, ctx) => {
-    return res(ctx.json(dummySession));
-  }),
-  rest.post("/api/logout", (req, res, ctx) => {
-    return res(ctx.status(200));
-  })
-);
+// const server = setupServer(
+//   rest.post("/api/login", (req, res, ctx) => {
+//     return res(ctx.json(dummySession));
+//   }),
+//   rest.post("/api/logout", (req, res, ctx) => {
+//     return res(ctx.status(200));
+//   })
+// );
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
 
 const AllTheProviders = ({ children }) => {
   return (
