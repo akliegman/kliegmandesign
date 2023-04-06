3a1bb595f241a3ff05faef7b08dda6992d0acf3e
Adam Kliegman <akliegman@users.noreply.github.com>
2023-04-06 12:04:18 -0400

    Implement github action on pull request to run tests. (#5)

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,9 +1,8 @@
 import "@testing-library/jest-dom";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
 import { render } from "@testing-library/react";
 import { AuthContext } from "./context/AuthContext";
 import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
 
 jest.mock("axios", () => ({
   create: jest.fn(() => ({
@@ -13,29 +12,6 @@ jest.mock("axios", () => ({
     delete: jest.fn(),
   })),
 }));
-
-const dummySession = {
-  user: {
-    email: "test@example.com",
-    firstName: "John",
-    lastName: "Doe",
-  },
-  token: "123abc",
-  isNew: false,
-};
-
-const server = setupServer(
-  rest.post("/api/login", (req, res, ctx) => {
-    return res(ctx.json(dummySession));
-  }),
-  rest.post("/api/logout", (req, res, ctx) => {
-    return res(ctx.status(200));
-  })
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
 
 const AllTheProviders = ({ children }) => {
   return (
