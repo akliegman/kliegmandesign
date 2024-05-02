97e1aecf803efbd5c6e8e64f6ab0cce88d295f17
Adam Kliegman <kliegman14@gmail.com>
2024-05-01 21:18:38 -0400

    fix(Video): better mobile handling,;

--- client/src/components/Homepage/Masthead/Masthead.jsx
+++ client/src/components/Homepage/Masthead/Masthead.jsx
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import { Button } from "../../reusables";
 
 import styles from "./Masthead.module.less";
 
 export const Masthead = ({ data }) => {
+  const [videoMounted, setVideoMounted] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      setVideoMounted(true);
+    } else {
+      setVideoMounted(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      setVideoMounted(true);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.Container}>
-      <div className={styles.VideoContainer}>
-        <video autoPlay muted loop className={styles.Video}>
-          <source src={data.video} type="video/mp4" />
-        </video>
-      </div>
+      {videoMounted ? (
+        <div className={styles.VideoContainer}>
+          <video autoPlay muted loop playsInline className={styles.Video}>
+            <source src={data.video} type="video/mp4" />
+          </video>
+        </div>
+      ) : (
+        <div className={styles.MobilePlaceholder} />
+      )}
       <div className={styles.Image}>
         <img src={data.image} alt="Adam Kliegman" height="200" width="200" />
       </div>
